f649b99a089a06db8a398af268d369376aff2824
BolleanCC <songtao19980820@outlook.com>
2025-09-09 15:24:03 -0600

    Added navbar

---
+++ shadcn-dashboard/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { CirclePlus } from "lucide-react";
+
+const Homepage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      {/* <Button size="xl" className="rounded-full text-custom-color">
+        <CirclePlus />
+        Click me */}
+      {/* </Button> */}
+    </div>
+  );
+};
+
+export default Homepage;
