6e69c5c1a1a7083a6d73504feab41d88c52b0e10
BolleanCC <songtao19980820@outlook.com>
2025-10-03 11:15:51 -0600

    Added calendar into to do list

--- shadcn-dashboard/src/components/TodoList.tsx
+++ shadcn-dashboard/src/components/TodoList.tsx
@@ -1,14 +1,38 @@
+"use client"
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Checkbox } from "@/components/ui/checkbox"
 import { Card } from "./ui/card";
+import { Popover, PopoverTrigger, PopoverContent } from "./ui/popover";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "./ui/calendar";
+import { format } from "date-fns";
 
 const TodoList = () => {
+
+    const [date, setDate] = useState<Date | undefined>(new Date());
+    const [open, setOpen] = useState(false);
     return (
         <div className="">
-            Calendar
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button variant="outline" className="w-full">
+                        <CalendarIcon />
+                        {date ? format(date, "PPP") : <span>Select date</span>}
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                    <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={(date) => { setDate(date); setOpen(false); }}
+                        className="rounded-lg border"
+                    />
+                </PopoverContent>
+            </Popover>
             <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto">
                 <div className="flex flex-col gap-4">
-
                     <Card className="p-4">
                         <div className="flex items-center gap-4">
                             <Checkbox id="item-1">
