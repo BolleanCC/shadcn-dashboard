62e03c5a56a03337fac7e7393d1403fbaf3617b6
BolleanCC <songtao19980820@outlook.com>
2025-11-04 01:33:59 -0700

    added user info for single user page

--- shadcn-dashboard/src/app/users/[username]/page.tsx
+++ shadcn-dashboard/src/app/users/[username]/page.tsx
@@ -1,7 +1,9 @@
 import CardList from "@/components/CardList";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator, BreadcrumbPage } from "@/components/ui/breadcrumb";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
-import { BadgeCheck } from "lucide-react";
+import { Progress } from "@/components/ui/progress";
+import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 const SingleUserPage = () => {
     return (
@@ -36,11 +38,83 @@ const SingleUserPage = () => {
                                     <p className="text-sm text-muted-foreground">This user has been verified by the platform.</p>
                                 </HoverCardContent>
                             </HoverCard>
+                            <HoverCard>
+                                <HoverCardTrigger>
+                                    <Shield
+                                        size={36}
+                                        className="rounded-full bg-green-800/30 border-1 border-green-800/50 p-2"
+                                    />
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <h1 className="font-bold mb-2">Admin</h1>
+                                    <p className="text-sm text-muted-foreground">
+                                        Admin users have access to all features and can manage
+                                        users.
+                                    </p>
+                                </HoverCardContent>
+                            </HoverCard>
+                            <HoverCard>
+                                <HoverCardTrigger>
+                                    <Candy
+                                        size={36}
+                                        className="rounded-full bg-yellow-500/30 border-1 border-yellow-500/50 p-2"
+                                    />
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <h1 className="font-bold mb-2">Awarded</h1>
+                                    <p className="text-sm text-muted-foreground">
+                                        This user has been awarded for their contributions.
+                                    </p>
+                                </HoverCardContent>
+                            </HoverCard>
+                            <HoverCard>
+                                <HoverCardTrigger>
+                                    <Citrus
+                                        size={36}
+                                        className="rounded-full bg-orange-500/30 border-1 border-orange-500/50 p-2"
+                                    />
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <h1 className="font-bold mb-2">Popular</h1>
+                                    <p className="text-sm text-muted-foreground">
+                                        This user has been popular in the community.
+                                    </p>
+                                </HoverCardContent>
+                            </HoverCard>
                         </div>
                     </div>
                     {/* INFORMATION CONTAINER*/}
                     <div className="bg-primary-foreground rounded-lg p-4">
-                        Info
+                        <h1 className="text-xl sm:text-2xl font-semibold">User Information</h1>
+                        <div className="space-y-4 mt-4">
+                            <div className="flex flex-col gap-2 mb-8">
+                                <p className="text-sm text-muted-foreground">Profile Completion</p>
+                                <Progress value={66} />
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="font-bold">Username</span>
+                                <span>Songtao</span>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="font-bold">Email:</span>
+                                <span>john.doe@gmail.com</span>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="font-bold">Phone:</span>
+                                <span>+1 234 5678</span>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="font-bold">Location:</span>
+                                <span>New York, NY</span>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="font-bold">Role:</span>
+                                <Badge>Admin</Badge>
+                            </div>
+                            <p className="text-sm text-muted-foreground">
+                                Joined on <span className="font-bold">2021-01-01</span>
+                            </p>
+                        </div>
                     </div>
                     {/* CARD LIST CONTAINER*/}
                     <div className="bg-primary-foreground rounded-lg p-4">
